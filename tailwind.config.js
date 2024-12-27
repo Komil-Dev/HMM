b216ad81f41ca4627461cda6faa64740d792d325
Komil-axiy2 <Komil7024868@gmail.com>
2024-12-27 20:49:50 +0500

    well  done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"], // Adjust paths based on your project
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        robotoCondensed: ["'Roboto Condensed'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
