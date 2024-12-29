f701d7ec00c846b16ebbf3494d809d22da5f079e
Komil-axiy2 <Komil7024868@gmail.com>
2024-12-29 14:49:10 +0500

    well  done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,20 @@ module.exports = {
   },
   plugins: [],
 };
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 0.5s ease-in-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
